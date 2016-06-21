9614b94b34d0933002ca77ed9dc323dbb17c8665
migulios <mijseiton@gmail.com>
2016-06-21 15:52:37 +0100

    Update pcaDFDATA.m

--- pcaDFDATA.m
+++ pcaDFDATA.m
@@ -1,16 +1,21 @@
-
+%# Function for the pca by cluster
+% we need the imaging data and the numbers of clusters
 function imData=pcaDFDATA(imData, n_clusters)
 
+%# Smoothing the signal before pca
 n_smooth=6;
 
 for i = 1: size(imData.dF_DATA,1)
     dF_DATA_smooth (i,:) = smooth (imData.dF_DATA(i,:),n_smooth);
 end
 
+%# pca 
 [pca_coeff,pca_values,~,~,pca_var]=pca(dF_DATA_smooth);
 
+%CLustering 
 clusterColour=colormap(jet(n_clusters));
 clusters_id=kmeans(pca_values,n_clusters);
+
 figure;
 for i=1:n_clusters
     plot(pca_values(clusters_id==i,1),pca_values(clusters_id==i,2),'.','color',clusterColour(i,:))
